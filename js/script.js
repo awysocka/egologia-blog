c6ae6fc648d9d35988f478010387d6faacc198e3
awysocka <wysocka.anna88@gmail.com>
2020-05-11 00:46:55 +0200

    EB-9 Add hamburger menu

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// ---MOBILE MENU---
+
+const hamburgerIcon = document.getElementById('hamburger');
+const nav = document.getElementById('navigation');
+const navLinks = document.getElementsByClassName('navigation__item');
+const navList = document.getElementById('nav-list');
+
+// open and close mobile menu after 'click' on hamburger
+  hamburgerIcon.onclick = () => {
+  hamburgerIcon.classList.toggle('hamburger--active');
+  nav.classList.toggle('navigation--active');
+  navList.classList.toggle('navigation__list--active');
+};
+
+// close mobile menu after 'click' on menu link
+const closeMenu = () => {
+
+  hamburgerIcon.classList.remove('hamburger--active');
+  nav.classList.remove('navigation--active');
+  navList.classList.remove('navigation__list--active');
+};
+
+for (let navLink of navLinks) {
+  navLink.onclick = closeMenu;
+}
